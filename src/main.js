a66b988b3ddab9d32e2f8f406ff96bc55485cbc5
VrecTangleRNG <vigoeka5@gmail.com>
2026-09-11 22:22:13 +0700

    Draw textures

--- src/main.js
+++ src/main.js
@@ -1,29 +1,35 @@
+// Import main dependencies
 import { Application, Assets, Sprite } from 'pixi.js';
 
-// Initialize the PixiJS Application
-const app = new Application();
-await app.init({ 
-    resizeTo: window, 
-    backgroundColor: 0x1099bb 
-});
 
-// Append the canvas element(like <div> element) to the HTML document
-document.body.appendChild(app.canvas);
+(async () => {
 
-// Load a texture (using a placeholder image for testing)
-const texture = await Assets.load(/* Load texture in here */);
+	/*----[[ Initializations ]]----*/
+	// Init application
+	const app = new Application();
+	await app.init({ 
+	    resizeTo: window, 
+	    backgroundColor: 0x1099bb 
+	});
+	document.body.appendChild(app.canvas);
 
-// Create a sprite from the texture and center it
-const sprite = new Sprite(texture);
-bunny.anchor.set(0.5);				// Set center point (to half-texture)
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
+	// Init game objects
+	const playerSprite = await Assets.load('./assets/textures/char0.png');
+	const gunSPrite = await Assets.load('./assets/textures/glock.png');
+	const particleSprite = await Assets.load('./assets/textures/particle.png');
+	const player = new Sprite(playerSprite);
+	const gun = new Sprite(gunSPrite);
+	const bullet = new Sprite(particleSprite);
+	app.stage.addChild(player);
+	app.stage.addChild(gun);
+	app.stage.addChild(bullet);
 
-// Add sprite to screen canvas
-app.stage.addChild(sprite);
 
-// Add a simple animation ticker (main game loop)
-app.ticker.add((time) => {
-    // Rotate the bunny continuously
-    sprite.rotation += 0.1 * time.deltaTime;
-});
+	/*----[[ Updates ]]----*/
+	app.ticker.add((time) => {
+	});
+
+
+	/*----[[ Signals ]]----*/
+})();
+
